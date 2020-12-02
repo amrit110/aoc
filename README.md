74cb455240620bcd2fffb4ee0d15bd60033769c1
Amrit K <amrit110@gmail.com>
2020-12-02 05:25:33 -0500

    Use aocd to fetch input in bootstrap script

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # aoc
 Advent of Code
+
+
+* Setup python virtual environment:
+
+```bash
+virtualenv venv
+source venv/bin/activate
+```
+
+* Install dependencies:
+
+```bash
+pip install --upgrade pip
+pip install -r requirements.txt
+```
+
+* For using [aocd package](https://github.com/wimglenn/advent-of-code-data) 
+to fetch input, export session ID:
+
+```bash
+export AOC_SESSION=<session_id>
+```
--- bootstrap.py
+++ bootstrap.py
@@ -3,8 +3,9 @@
 
 import argparse
 import os
-from os.path import join
+from os.path import dirname, realpath, join
 from shutil import copyfile
+from aocd import get_data
 
 
 def parse_args():
@@ -15,22 +16,30 @@ def parse_args():
     return parser.parse_args()
 
 
+def download_input(year, day, dst_input_path):
+    try:
+        data = get_data(year=year, day=day)
+        print(f'Fetched input year-{year}, day-{day}')
+    except:
+        data = ''
+
+    with open(dst_input_path, 'w') as f:
+        f.write(data)
+
+
 def bootstrap_solution(year, day):
     target_dir = join(f'{year}', f'day{day}')
     os.makedirs(target_dir, exist_ok=True)
-    skeleton_script_path = join(target_dir, 'solve.py')
+    dst = join(target_dir, 'solve.py')
     
-    if not os.path.exists(skeleton_script_path):
-        template_script_path = join('.', 'template.py')
-        copyfile(template_script_path, skeleton_script_path)
-
+    if not os.path.exists(dst):
+        src = join(dirname(realpath(__file__)), 'template.py')
+        copyfile(src, dst)
         print(f'Created skeleton for year-{year}, day-{day}')
 
-    # Create empty input text file
-    input_file_path = join(target_dir, 'input.txt')
-    if not os.path.exists(input_file_path):
-        with open(input_file_path, 'w') as f:
-            pass
+    dst_input_path = join(target_dir, 'input.txt')
+    if not os.path.exists(dst_input_path):
+        download_input(year, day, dst_input_path)
 
 
 def main():
