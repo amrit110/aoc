1738f35a8824acb52371927b70886b9b054b4d4c
Amrit K <amrit110@gmail.com>
2021-02-10 10:40:35 -0500

    Cleaner

--- .github/workflows/trigger_timer.sh
+++
@@ -1,5 +0,0 @@
-curl -X POST \
-    -H "Accept: application/vnd.github.v3+json" \
-    -H "Authorization: token ${secrets.GITHUB_TOKEN}" \
-    --data '{"event_type": "aoc_timer", "client_payload": { "year": "2020", "day": "1"}}' \
-    https://api.github.com/repos/hoxovic/aoc_timing/dispatches
